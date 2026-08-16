dcc4002b559c3398f3b13757d1e4052329331437
Павел Лайков <i-likesoft@yandex.ru>
2026-08-16 08:20:56 +0300

    Добавлен модуль processing.py
    Signed-off-by: Павел Лайков <i-likesoft@yandex.ru>

--- src/widget.py
+++ src/widget.py
@@ -1,22 +1,15 @@
-# Импортируем функции get_mask_card_number и get_mask_account из модуля masks.py
-from masks import get_mask_card_number
-from masks import get_mask_account
-
-# Импорт модуля re
+# Импорт встроенных модулей Python - re и datetime
 import re
-
-# Импорт модуля datetime
 from datetime import datetime
 
-def mask_account_card(account_card: str) -> str:
-    """ Маскирует полученный номер карты или счета.
-    Принимает один аргумент — строку, содержащую тип и номер карты или
-    номер счёта, и возвращает строку с замаскированным номером.
-    Использует функции get_mask_card_number и get_mask_account из модуля masks. """
-    # card_number - пример номера карты
-    # account_number - пример номера счета
+# Импорт функций проекта из masks
+from src.masks import get_mask_account, get_mask_card_number
 
-    if "Счёт" in account_card:  # Создается маска для счета
+
+def mask_account_card(account_card: str) -> str:
+    """Маскирует полученный номер карты или счета"""
+
+    if "Счет" in account_card:  # Создается маска для счета
         letters_part = "".join(re.findall(r"\D+", account_card))
         numbers_part = "".join(re.findall(r"\d+", account_card))
         hidden_account_card = f"{letters_part} {get_mask_account(numbers_part)}"
@@ -28,9 +21,9 @@ def mask_account_card(account_card: str) -> str:
     return hidden_account_card
 
 
-def get_date(date: str) -> str:
-    """ Функция принимает на вход строку в формате "2026-07-27T02:26:18.671407"
-    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("27.07.2026").
-    """
-    date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
-    return date.strftime("%d.%m.%Y")
+def get_date(date_iso_8601: str) -> str:
+    """Конвертирует дату из международного стандарта в обычный формат 'ДД.ММ.ГГГГ'"""
+
+    formatted_date = datetime.strptime(date_iso_8601, "%Y-%m-%dT%H:%M:%S.%f")
+
+    return formatted_date.strftime("%d.%m.%Y")
